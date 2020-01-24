99ea60a50c37da5518db41e768b81a2c51f5b972
ldi_hwen <h.wen@ldi.co.jp>
2020-01-24 15:41:35 +0900

    update code

--- app/src/main/java/com/hyejeanmoon/asynctackdemo/MainActivity.kt
+++ app/src/main/java/com/hyejeanmoon/asynctackdemo/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.hyejeanmoon.asynctackdemo
 
+import android.annotation.SuppressLint
 import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.material.snackbar.Snackbar
 import com.hyejeanmoon.asynctackdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,38 +18,51 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.btn.setOnClickListener {
-            val result = MyTask().execute("Hello World").get()
-            Snackbar.make(binding.root, result, Snackbar.LENGTH_LONG).show()
+            MyTask().execute("Hello World")
         }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class MyTask : AsyncTask<String, Int, String>() {
+
+        // 必须重写
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Log.d("MyTask", "onPreExecute")
+            binding.textView.text = "MyTask onPreExecute"
+        }
+
+        // 必须重写
+        override fun doInBackground(vararg params: String?): String {
+            Log.d("MyTask", "doInBackground")
+
+            for (i in 0..100) {
+                Thread.sleep(50)
+                publishProgress(i)
+            }
+
+            var result = "PEACE!"
+            params[0]?.also {
+                result = "$it $result"
+            }
+
+            return result
+        }
+
+        // 不必须重写
+        override fun onProgressUpdate(vararg values: Int?) {
+            super.onProgressUpdate(*values)
+
+            binding.textView.text = "${values[0]} %"
+        }
+
+        // 不必须重写
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            Log.d("MyTask", "onPostExecute")
+            binding.textView.text = "MyTask onPostExecute"
+        }
+
     }
 }
 
-class MyTask() : AsyncTask<String, String, String>() {
-
-    // 必须重写
-    override fun onPreExecute() {
-        super.onPreExecute()
-        Log.d("MyTask", "onPreExecute")
-    }
-
-    // 必须重写
-    override fun doInBackground(vararg params: String?): String {
-        Log.d("MyTask", "doInBackground")
-
-        Thread.sleep(2000)
-
-        var result = "PEACE!"
-        params[0]?.also {
-            result = "$it $result"
-        }
-
-        return result
-    }
-
-    // 不必须重写
-    override fun onPostExecute(result: String?) {
-        super.onPostExecute(result)
-        Log.d("MyTask", "onPostExecute")
-    }
-
-}
